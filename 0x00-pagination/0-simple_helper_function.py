b3bb57d6e1b7a00c62ef02db63c459a192a81a02
yoojen <eugeneemma7@gmail.com>
2023-12-24 18:40:55 +0200

    fixed zeroError validation

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,10 +4,10 @@
 from typing import Tuple
 
 
-def index_range(page:int, page_size: int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """return the starting index and ending index"""
     if page and page_size != 0:
         start_index = (page - 1) * page_size
-        end_index =  page * page_size
+        end_index = page * page_size
 
         return start_index, end_index
