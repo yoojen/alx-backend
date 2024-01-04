a4b1322767eaf7ba53a84b16fcf314e5b9ee15a2
yoojen <eugeneemma7@gmail.com>
2024-01-05 00:16:55 +0200

    cache storage implemented

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+BasicCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BasicCaching):
+    """implement basic caching algorithm"""
+
+    def put(self, key, item):
+        """put new item in the cache storage"""
+        if key or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """retrieve an item from the cache storage"""
+        if key is None or KeyError:
+            return None
+        return self.cache_data.get(key)
