76f61c6cdaf25d1d391a08712b625302574b148d
Eugene <140322568+yoojen@users.noreply.github.com>
2024-01-09 22:49:34 +0200

    Create 0-app.py

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """return string on the home of web"""
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000)
